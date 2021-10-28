4d7cc67f3456659e951a8de3db775ec3296cecc5
Stackbit Code Editor <projects@stackbit.com>
2021-10-28 02:57:04 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -11,7 +11,7 @@ export default class Page extends React.Component {
         const page = _.get(this.props, 'page');
         const title = _.get(page, 'title');
         const markdownContent = _.get(page, 'markdown_content');
-        const markdownContent = _.get(page, 'markdown_content');
+        const markdownContent2 = _.get(page, 'markdown_content');
 
         return (
             <Layout page={page} config={config}>
